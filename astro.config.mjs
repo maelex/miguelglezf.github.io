0f16aa8cbee1442ee31f18081face426a2ac41aa
Miguel Alejandro González Fernández <miguel-aelex005@hotmail.com>
2025-07-17 23:24:47 -0600

    Workiflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maelex.github.io/miguelglezf.github.io/',
-  //base: 'miguelglezf.github.io',
+  base: 'miguelglezf.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
