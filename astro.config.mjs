e7eb6f4bb8d7a767a2db78c1af0a9d446915f2c8
Miguel Alejandro González Fernández <miguel-aelex005@hotmail.com>
2025-07-17 22:24:08 -0600

    Setting site vairable to deply

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maelex.github.io/miguelglezf.github.io/',
+  //base: 'miguelglezf.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
